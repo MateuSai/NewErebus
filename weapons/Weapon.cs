630ddf1c7079611883323bf8c797da3649ec9d9c
WekufuStudios <wekufu.studios@gmail.com>
2024-04-05 15:06:48 -0300

    nuevas anims fists

--- weapons/Weapon.cs
+++ weapons/Weapon.cs
@@ -4,31 +4,32 @@ namespace Erebus.Weapons;
 
 abstract public partial class Weapon : Node2D
 {
-    public Node2D LeftHand;
-    public Node2D RightHand;
+	public Node2D LeftHand;
+	public Node2D RightHand;
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 
-        // This is only for reference at the moment of making the animations
-        GetNode<Sprite2D>("PlayerReference").QueueFree();
+		// This is only for reference at the moment of making the animations
+		GetNode<Sprite2D>("PlayerReference").QueueFree();
+		GetNode<Sprite2D>("PlayerReferenceUp").QueueFree();
 
-        LeftHand = GetNode<Node2D>("%LeftHand");
-        RightHand = GetNode<Node2D>("%RightHand");
-    }
+		LeftHand = GetNode<Node2D>("%LeftHand");
+		RightHand = GetNode<Node2D>("%RightHand");
+	}
 
-    public void Move(Vector2 dir)
-    {
-        Rotation = dir.Angle();
+	public void Move(Vector2 dir)
+	{
+		Rotation = dir.Angle();
 
-        if (Scale.Y == 1 && Vector2.Right.Rotated(Rotation).X < 0)
-        {
-            Scale = new Vector2(Scale.X, -1);
-        }
-        else if (Scale.Y == -1 && Vector2.Right.Rotated(Rotation).X > 0)
-        {
-            Scale = new Vector2(Scale.X, 1);
-        }
-    }
+		if (Scale.Y == 1 && Vector2.Right.Rotated(Rotation).X < 0)
+		{
+			Scale = new Vector2(Scale.X, -1);
+		}
+		else if (Scale.Y == -1 && Vector2.Right.Rotated(Rotation).X > 0)
+		{
+			Scale = new Vector2(Scale.X, 1);
+		}
+	}
 }
--- weapons/Weapons.cs
+++ weapons/Weapons.cs
@@ -7,30 +7,30 @@ using System.Linq;
 
 public partial class Weapons : Node2D
 {
-    protected Weapon CurrentWeapon = null;
-    protected List<Weapon> WeaponsArray = new();
+	protected Weapon CurrentWeapon = null;
+	protected List<Weapon> WeaponsArray = new();
 
-    /// <summary>
-    /// Execute this function to initialize the class
-    /// </summary>
-    public virtual void Start()
-    {
+	/// <summary>
+	/// Execute this function to initialize the class
+	/// </summary>
+	public virtual void Start()
+	{
 
-    }
+	}
 
-    public void PickUpWeapon(Weapon weapon)
-    {
-        _ = WeaponsArray.Append(weapon);
-        AddChild(weapon);
-    }
+	public void PickUpWeapon(Weapon weapon)
+	{
+		_ = WeaponsArray.Append(weapon);
+		AddChild(weapon);
+	}
 
-    public void MoveCurrentWeapon(Vector2 dir)
-    {
-        CurrentWeapon.Move(dir);
-    }
+	public void MoveCurrentWeapon(Vector2 dir)
+	{
+		CurrentWeapon.Move(dir);
+	}
 
-    public virtual void SetCurrentWeapon(Weapon newWeapon)
-    {
-        CurrentWeapon = newWeapon;
-    }
+	public virtual void SetCurrentWeapon(Weapon newWeapon)
+	{
+		CurrentWeapon = newWeapon;
+	}
 }
