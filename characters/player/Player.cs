504c4e190a6f6a26ca9a18d8e343e8459b0e7975
WekufuStudios <wekufu.studios@gmail.com>
2024-06-08 13:51:06 -0400

    jjbb kjm

--- characters/player/Player.cs
+++ characters/player/Player.cs
@@ -8,70 +8,70 @@ using Godot;
 
 public partial class Player : Character
 {
-    public Vector2 MousePosition = Vector2.Zero;
-    public Vector2 MouseDirection = Vector2.Zero;
+	public Vector2 MousePosition = Vector2.Zero;
+	public Vector2 MouseDirection = Vector2.Zero;
 
-    private enum FacingDir
-    {
-        TopLeft,
-        TopRight,
-        BottomLeft,
-        BottomRight,
-    }
-    private FacingDir _facingDir = FacingDir.BottomRight;
-    private enum HorizontalFacingDir
-    {
-        Left,
-        Right,
-    }
-    private HorizontalFacingDir _horizontalFacingDir = HorizontalFacingDir.Left;
-    private enum VerticalFacingDir
-    {
-        Top,
-        Bottom,
-    }
-    private VerticalFacingDir _verticalFacingDir = VerticalFacingDir.Bottom;
+	private enum FacingDir
+	{
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight,
+	}
+	private FacingDir _facingDir = FacingDir.BottomRight;
+	private enum HorizontalFacingDir
+	{
+		Left,
+		Right,
+	}
+	private HorizontalFacingDir _horizontalFacingDir = HorizontalFacingDir.Left;
+	private enum VerticalFacingDir
+	{
+		Top,
+		Bottom,
+	}
+	private VerticalFacingDir _verticalFacingDir = VerticalFacingDir.Bottom;
 
-    private PlayerStateMachine _stateMachine;
+	private PlayerStateMachine _stateMachine;
 
-    private Sprite2D _backpackSprite;
-    private Sprite2D _legsArmorSprite;
-    private Sprite2D _sprite;
-    private AnimationPlayer _animationPlayer;
-    private PlayerWeapons _weapons;
-    private ItemsOnFloorDetector _itemsOnFloorDetector;
+	private Sprite2D _backpackSprite;
+	private Sprite2D _legsArmorSprite;
+	private Sprite2D _sprite;
+	private AnimationPlayer _animationPlayer;
+	private PlayerWeapons _weapons;
+	private ItemsOnFloorDetector _itemsOnFloorDetector;
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 
-        _backpackSprite = GetNode<Sprite2D>("Backpack");
-        _legsArmorSprite = GetNode<Sprite2D>("LegsArmorSprite");
-        _sprite = GetNode<Sprite2D>("Sprite2D");
-        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-        _weapons = GetNode<PlayerWeapons>("Weapons");
-        _itemsOnFloorDetector = GetNode<ItemsOnFloorDetector>("ItemsOnFloorDetector");
+		_backpackSprite = GetNode<Sprite2D>("Backpack");
+		_legsArmorSprite = GetNode<Sprite2D>("LegsArmorSprite");
+		_sprite = GetNode<Sprite2D>("Sprite2D");
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		_weapons = GetNode<PlayerWeapons>("Weapons");
+		_itemsOnFloorDetector = GetNode<ItemsOnFloorDetector>("ItemsOnFloorDetector");
 
-        _stateMachine = GetNode<PlayerStateMachine>("StateMachine");
+		_stateMachine = GetNode<PlayerStateMachine>("StateMachine");
 
-        _weapons.Start();
+		_weapons.Start();
 
-        GetNode<Globals>("/root/Globals").Player = this;
-    }
+		GetNode<Globals>("/root/Globals").Player = this;
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        base._Input(@event);
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
 
-        MovDirection.X = Input.GetAxis("move_left", "move_right");
-        MovDirection.Y = Input.GetAxis("move_up", "move_down");
-    }
+		MovDirection.X = Input.GetAxis("move_left", "move_right");
+		MovDirection.Y = Input.GetAxis("move_up", "move_down");
+	}
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
 
-        /* if (MousePosition.X > 1 && Scale.X == -1)
+		/* if (MousePosition.X > 1 && Scale.X == -1)
 		{
 			Scale = new Vector2((float)1.0, Scale.Y);
 			_rightIK.FlipBendDirection = true;
@@ -84,155 +84,155 @@ public partial class Player : Character
 			_leftIK.FlipBendDirection = false;
 		} */
 
-        if (MouseDirection.Y >= 0)
-        {
-            if (MouseDirection.X < 0 && _facingDir != FacingDir.BottomLeft)
-            {
-                SetFacingDir(FacingDir.BottomLeft);
-            }
+		if (MouseDirection.Y >= 0)
+		{
+			if (MouseDirection.X < 0 && _facingDir != FacingDir.BottomLeft)
+			{
+				SetFacingDir(FacingDir.BottomLeft);
+			}
 
-            else if (MouseDirection.X > 0 && _facingDir != FacingDir.BottomRight)
-            {
-                SetFacingDir(FacingDir.BottomRight);
-            }
-        }
-        else
-        {
-            if (MouseDirection.X < 0 && _facingDir != FacingDir.TopLeft)
-            {
-                SetFacingDir(FacingDir.TopLeft);
-            }
-            else if (MouseDirection.X > 0 && _facingDir != FacingDir.TopRight)
-            {
-                SetFacingDir(FacingDir.TopRight);
-            }
-        }
-    }
+			else if (MouseDirection.X > 0 && _facingDir != FacingDir.BottomRight)
+			{
+				SetFacingDir(FacingDir.BottomRight);
+			}
+		}
+		else
+		{
+			if (MouseDirection.X < 0 && _facingDir != FacingDir.TopLeft)
+			{
+				SetFacingDir(FacingDir.TopLeft);
+			}
+			else if (MouseDirection.X > 0 && _facingDir != FacingDir.TopRight)
+			{
+				SetFacingDir(FacingDir.TopRight);
+			}
+		}
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        MousePosition = _weapons.GetLocalMousePosition();
-        //MouseDirection = (_weapons.GetLocalMousePosition() + new Vector2(-5, -1)).Normalized();
-        MouseDirection = MousePosition.Normalized();
-        //GetNode<Node2D>("IKRightHandTarget").Position = GetLocalMousePosition();
-        //GetNode<Node2D>("IKLeftHandTarget").Position = GetLocalMousePosition();
-        _stateMachine.Update(delta);
+	public override void _PhysicsProcess(double delta)
+	{
+		MousePosition = _weapons.GetLocalMousePosition();
+		//MouseDirection = (_weapons.GetLocalMousePosition() + new Vector2(-5, -1)).Normalized();
+		MouseDirection = MousePosition.Normalized();
+		//GetNode<Node2D>("IKRightHandTarget").Position = GetLocalMousePosition();
+		//GetNode<Node2D>("IKLeftHandTarget").Position = GetLocalMousePosition();
+		_stateMachine.Update(delta);
 
-        base._PhysicsProcess(delta);
+		base._PhysicsProcess(delta);
 
-        _weapons.MoveCurrentWeapon(MouseDirection);
-    }
+		_weapons.MoveCurrentWeapon(MouseDirection);
+	}
 
-    private void UpdateShoulderBones()
-    {
-        switch (_facingDir)
-        {
-            case FacingDir.BottomRight:
-                break;
-            case FacingDir.TopRight:
-                break;
-            case FacingDir.BottomLeft:
-                break;
-            case FacingDir.TopLeft:
-                break;
-        }
-    }
+	private void UpdateShoulderBones()
+	{
+		switch (_facingDir)
+		{
+			case FacingDir.BottomRight:
+				break;
+			case FacingDir.TopRight:
+				break;
+			case FacingDir.BottomLeft:
+				break;
+			case FacingDir.TopLeft:
+				break;
+		}
+	}
 
-    private bool IsFacingRight()
-    {
-        return !_sprite.FlipH;
-    }
+	private bool IsFacingRight()
+	{
+		return !_sprite.FlipH;
+	}
 
-    private void SetFacingDir(FacingDir newFacingDir)
-    {
-        if ((newFacingDir == FacingDir.TopLeft || newFacingDir == FacingDir.BottomLeft) && (_facingDir == FacingDir.TopRight || _facingDir == FacingDir.BottomRight))
-        {
-            SetHorizontalFacingDir(HorizontalFacingDir.Left);
-        }
-        else if ((newFacingDir == FacingDir.TopRight || newFacingDir == FacingDir.BottomRight) && (_facingDir == FacingDir.TopLeft || _facingDir == FacingDir.BottomLeft))
-        {
-            SetHorizontalFacingDir(HorizontalFacingDir.Right);
-        }
+	private void SetFacingDir(FacingDir newFacingDir)
+	{
+		if ((newFacingDir == FacingDir.TopLeft || newFacingDir == FacingDir.BottomLeft) && (_facingDir == FacingDir.TopRight || _facingDir == FacingDir.BottomRight))
+		{
+			SetHorizontalFacingDir(HorizontalFacingDir.Left);
+		}
+		else if ((newFacingDir == FacingDir.TopRight || newFacingDir == FacingDir.BottomRight) && (_facingDir == FacingDir.TopLeft || _facingDir == FacingDir.BottomLeft))
+		{
+			SetHorizontalFacingDir(HorizontalFacingDir.Right);
+		}
 
-        if ((newFacingDir == FacingDir.TopLeft || newFacingDir == FacingDir.TopRight) && (_facingDir == FacingDir.BottomLeft || _facingDir == FacingDir.BottomRight))
-        {
-            SetVerticalFacingDir(VerticalFacingDir.Top);
-        }
-        else if ((newFacingDir == FacingDir.BottomLeft || newFacingDir == FacingDir.BottomRight) && (_facingDir == FacingDir.TopLeft || _facingDir == FacingDir.TopRight))
-        {
-            SetVerticalFacingDir(VerticalFacingDir.Bottom);
-        }
+		if ((newFacingDir == FacingDir.TopLeft || newFacingDir == FacingDir.TopRight) && (_facingDir == FacingDir.BottomLeft || _facingDir == FacingDir.BottomRight))
+		{
+			SetVerticalFacingDir(VerticalFacingDir.Top);
+		}
+		else if ((newFacingDir == FacingDir.BottomLeft || newFacingDir == FacingDir.BottomRight) && (_facingDir == FacingDir.TopLeft || _facingDir == FacingDir.TopRight))
+		{
+			SetVerticalFacingDir(VerticalFacingDir.Bottom);
+		}
 
-        _facingDir = newFacingDir;
-        UpdateShoulderBones();
-    }
+		_facingDir = newFacingDir;
+		UpdateShoulderBones();
+	}
 
-    private void SetHorizontalFacingDir(HorizontalFacingDir newHorizontalFacingDir)
-    {
-        _horizontalFacingDir = newHorizontalFacingDir;
+	private void SetHorizontalFacingDir(HorizontalFacingDir newHorizontalFacingDir)
+	{
+		_horizontalFacingDir = newHorizontalFacingDir;
 
-        switch (_horizontalFacingDir)
-        {
-            case HorizontalFacingDir.Right:
-                _sprite.FlipH = false;
-                _backpackSprite.FlipH = false;
-                _legsArmorSprite.FlipH = false;
-                break;
-            case HorizontalFacingDir.Left:
-                _sprite.FlipH = true;
-                _backpackSprite.FlipH = true;
-                _legsArmorSprite.FlipH = true;
-                break;
-            default:
-                System.Diagnostics.Debug.Assert(false, "Invalid horizontal facing direction value");
-                break;
-        }
-    }
+		switch (_horizontalFacingDir)
+		{
+			case HorizontalFacingDir.Right:
+				_sprite.FlipH = false;
+				_backpackSprite.FlipH = false;
+				_legsArmorSprite.FlipH = false;
+				break;
+			case HorizontalFacingDir.Left:
+				_sprite.FlipH = true;
+				_backpackSprite.FlipH = true;
+				_legsArmorSprite.FlipH = true;
+				break;
+			default:
+				System.Diagnostics.Debug.Assert(false, "Invalid horizontal facing direction value");
+				break;
+		}
+	}
 
-    private void SetVerticalFacingDir(VerticalFacingDir newVerticalFacingDir)
-    {
-        _verticalFacingDir = newVerticalFacingDir;
+	private void SetVerticalFacingDir(VerticalFacingDir newVerticalFacingDir)
+	{
+		_verticalFacingDir = newVerticalFacingDir;
 
-        switch (_verticalFacingDir)
-        {
-            case VerticalFacingDir.Bottom:
-                MoveChild(_backpackSprite, _sprite.GetIndex() - 1);
-                break;
-            case VerticalFacingDir.Top:
-                MoveChild(_backpackSprite, _sprite.GetIndex() + 1);
-                break;
-            default:
-                System.Diagnostics.Debug.Assert(false, "Invalid vertical facing direction value");
-                break;
-        }
-    }
+		switch (_verticalFacingDir)
+		{
+			case VerticalFacingDir.Bottom:
+				MoveChild(_backpackSprite, _sprite.GetIndex() - 1);
+				break;
+			case VerticalFacingDir.Top:
+				MoveChild(_backpackSprite, _sprite.GetIndex() + 1);
+				break;
+			default:
+				System.Diagnostics.Debug.Assert(false, "Invalid vertical facing direction value");
+				break;
+		}
+	}
 
-    public void SetBackpack(Backpack backpack)
-    {
-        if (backpack == null)
-        {
-            _backpackSprite.Texture = null;
-        }
-        else
-        {
-            _backpackSprite.Texture = backpack.SpriteSheet;
-        }
-    }
+	public void SetBackpack(Backpack backpack)
+	{
+		if (backpack == null)
+		{
+			_backpackSprite.Texture = null;
+		}
+		else
+		{
+			_backpackSprite.Texture = backpack.SpriteSheet;
+		}
+	}
 
-    public void SetLegsArmor(LegsArmor legsArmor)
-    {
-        if (legsArmor == null)
-        {
-            _legsArmorSprite.Texture = null;
-        }
-        else
-        {
-            _legsArmorSprite.Texture = legsArmor.SpriteSheet;
-        }
-    }
+	public void SetLegsArmor(LegsArmor legsArmor)
+	{
+		if (legsArmor == null)
+		{
+			_legsArmorSprite.Texture = null;
+		}
+		else
+		{
+			_legsArmorSprite.Texture = legsArmor.SpriteSheet;
+		}
+	}
 
-    public System.Collections.Generic.List<ItemOnFloor> GetCloseItemsOnFloor()
-    {
-        return _itemsOnFloorDetector.GetItemsOnFloor();
-    }
+	public System.Collections.Generic.List<ItemOnFloor> GetCloseItemsOnFloor()
+	{
+		return _itemsOnFloorDetector.GetItemsOnFloor();
+	}
 }
--- weapons/Weapon.cs
+++ weapons/Weapon.cs
@@ -4,73 +4,73 @@ namespace Erebus.Weapons;
 
 abstract public partial class Weapon : Node2D
 {
-    public enum State
-    {
-        Idle,
-        Move,
-        Engage,
-    }
-    private State _state = State.Idle;
+	public enum State
+	{
+		Idle,
+		Move,
+		Engage,
+	}
+	private State _state = State.Idle;
 
-    private Node2D _leftHand;
-    private Node2D _rightHand;
-    private AnimationPlayer _animationPlayer;
+	private Node2D _leftHand;
+	private Node2D _rightHand;
+	private AnimationPlayer _animationPlayer;
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 
-        // This is only for reference at the moment of making the animations
-        GetNode<Sprite2D>("PlayerReference").QueueFree();
-        GetNode<Sprite2D>("PlayerReferenceUp").QueueFree();
+		// This is only for reference at the moment of making the animations
+		GetNode<Sprite2D>("PlayerReference").QueueFree();
+		GetNode<Sprite2D>("PlayerReferenceUp").QueueFree();
 
-        _leftHand = GetNode<Node2D>("%LeftHand");
-        _rightHand = GetNode<Node2D>("%RightHand");
-        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		_leftHand = GetNode<Node2D>("%LeftHand");
+		_rightHand = GetNode<Node2D>("%RightHand");
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 
-        //LeftHand.Hide();
-        //RightHand.Hide();
-    }
+		//LeftHand.Hide();
+		//RightHand.Hide();
+	}
 
-    public void Move(Vector2 dir)
-    {
-        Rotation = dir.Angle();
+	public void Move(Vector2 dir)
+	{
+		Rotation = dir.Angle();
 
-        if (Scale.Y == 1 && Vector2.Right.Rotated(Rotation).X < 0)
-        {
-            Scale = new Vector2(Scale.X, -1);
-        }
-        else if (Scale.Y == -1 && Vector2.Right.Rotated(Rotation).X > 0)
-        {
-            Scale = new Vector2(Scale.X, 1);
-        }
-    }
+		if (Scale.Y == 1 && Vector2.Right.Rotated(Rotation).X < 0)
+		{
+			Scale = new Vector2(Scale.X, -1);
+		}
+		else if (Scale.Y == -1 && Vector2.Right.Rotated(Rotation).X > 0)
+		{
+			Scale = new Vector2(Scale.X, 1);
+		}
+	}
 
-    public void Attack()
-    {
-        _animationPlayer.Play("Attack_01");
-        _state = State.Engage;
-    }
+	public void Attack()
+	{
+		_animationPlayer.Play("Attack_01");
+		_state = State.Engage;
+	}
 
-    public void StartIdleAnimation()
-    {
-        _animationPlayer.Play("Idle");
-        _state = State.Idle;
-    }
+	public void StartIdleAnimation()
+	{
+		_animationPlayer.Play("Idle");
+		_state = State.Idle;
+	}
 
-    public void StartMovingAnimation()
-    {
-        _animationPlayer.Play("Move");
-        _state = State.Move;
-    }
+	public void StartMovingAnimation()
+	{
+		_animationPlayer.Play("Move");
+		_state = State.Move;
+	}
 
-    public bool IsBusy()
-    {
-        return _animationPlayer.IsPlaying() && _animationPlayer.CurrentAnimation.StartsWith("Attack");
-    }
+	public bool IsBusy()
+	{
+		return _animationPlayer.IsPlaying() && _animationPlayer.CurrentAnimation.StartsWith("Attack");
+	}
 
-    public State GetState()
-    {
-        return _state;
-    }
+	public State GetState()
+	{
+		return _state;
+	}
 }
